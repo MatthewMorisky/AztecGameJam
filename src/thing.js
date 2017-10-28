ea2e42658457f5b47a6f01432db33032fdf329aa
Alex Jones <jones25519@gmail.com>
2017-10-28 15:12:26 -0700

    getting thing started

---
+++ src/thing.js
@@ -0,0 +1,14 @@
+class Thing {
+	constructor(x, y, xspeed, yspeed, hitbox, img, name) {
+		this.x = x;
+		this.y = y;
+		this.xspeed = xspeed;
+		this.yspeed = yspeed;
+		this.hitbox = hitbox;
+		this.img = img;
+		this.name = name;
+	}
+
+	
+
+}
