d1a5c5f5ee31441becf7367d29f0602e9284e32d
Matthew Morisky <matthew.morisky@gmail.com>
2017-10-28 08:45:39 -0700

    organized file structure

---
+++ sketch.js
@@ -0,0 +1,32 @@
+
+
+function setup() {
+
+	createCanvas(500, 500); 
+}
+
+function draw() {
+	background(51);
+	
+}
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+		
+	}
+	else if (keyCode === DOWN_ARROW) {
+		
+	}
+	else if (keyCode === LEFT_ARROW) {
+
+	}
+	else if (keyCode === RIGHT_ARROW) {
+
+	}
+	else if (keyCode === 32) { //Spacebar
+		
+	}
+}
+
+function keyReleased() {
+	
+}
