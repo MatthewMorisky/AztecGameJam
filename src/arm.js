2460269dee5a5f200f19f2adcc0fea59df39917c
Matthew Morisky <matthew.morisky@gmail.com>
2017-10-28 19:09:45 -0700

    arm physics

--- src/arm.js
+++ src/arm.js
@@ -1,11 +1,14 @@
 class Arm extends Thing {
 
 	constructor(x, y) {
-		super(x, y, 0, 0, new Hitbox(0,0,0,0), 'img/BoneAttack.gif', 'arm')
+		super(x, y, 0, 0, new Hitbox(0,0,40,40), 'img/BoneAttack.gif', 'arm')
 		this.x = player.x;
 		this.y = player.y;
 		this.distance = 0;
+		this.maxDistance = 20;
+
 		this.isRight = player.isRight;
+		this.isReturning = false;
 		if (this.isRight) {
 			this.xspeed = 10;
 		}
@@ -17,17 +20,26 @@ class Arm extends Thing {
 
 		super.update();
 		this.throw();
-
 	}
 throw() {
 	this.distance++
-		if (this.distance > 20) {
+		if (this.distance > this.maxDistance) {
+			this.isReturning = true;
 			this.distance = -21;
 			this.xspeed = -this.xspeed;
 		}
-		else if (this.distance == -1) {
-			this.delete();
+		else if (this.isReturning) {
+		
 		}
 	}
+collide(obj) {
+	if ((obj.name === 'player') && (this.isReturning)) {
+		player.hasArm = true;
+		this.delete();
+		this.isReturning = false;
+	}
+
+
+}
 }
 	
