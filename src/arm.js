e8adf0fa19f95ec42e164b5e30200035174aca82
Matthew Morisky <matthew.morisky@gmail.com>
2017-10-28 18:38:55 -0700

    throwing works

--- src/arm.js
+++ src/arm.js
@@ -1,27 +1,33 @@
 class Arm extends Thing {
 
 	constructor(x, y) {
-		super(x, y, 10, 0, new Hitbox(0,0,0,0), 'img/BoneAttack.gif', 'arm')
+		super(x, y, 0, 0, new Hitbox(0,0,0,0), 'img/BoneAttack.gif', 'arm')
 		this.x = player.x;
 		this.y = player.y;
 		this.distance = 0;
+		this.isRight = player.isRight;
+		if (this.isRight) {
+			this.xspeed = 10;
+		}
+		else if (!this.isRight) {
+			this.xspeed = -10;
+		}
 	}
 	update() {
 
 		super.update();
-		this.turnAbout();
+		this.throw();
 
 	}
-turnAbout() {
+throw() {
 	this.distance++
-	if (this.distance > 20) {
-		this.distance = -21;
-		this.xspeed = -this.xspeed;
-	}
-	else if (this.distance == -1) {
-		this.delete();
+		if (this.distance > 20) {
+			this.distance = -21;
+			this.xspeed = -this.xspeed;
+		}
+		else if (this.distance == -1) {
+			this.delete();
+		}
 	}
 }
 	
-
-}
