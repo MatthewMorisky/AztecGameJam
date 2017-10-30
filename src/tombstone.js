8f06c96be8284cdf9e10c8c859d616babc4d69e7
Matthew Morisky <matthew.morisky@gmail.com>
2017-10-29 18:05:29 -0700

    more names and got rid of excess files

---
+++ src/tombstone.js
@@ -0,0 +1,26 @@
+class Tombstone extends Thing 
+{
+	constructor(x, y)
+	{
+
+		super(x, y, 0, 0, new Hitbox(0, 0, 40, 40), 'img/tombstone2-1.png', 'tombstone');
+		this.sideLength = 40;
+	
+	}
+	update() {
+		super.update();
+		
+		
+		
+	}
+	
+	collide(obj)
+	{
+		console.log(obj.name);
+		if(obj.name === 'arm' || obj.name === 'player')
+		{
+			this.changeImage('img/tombstone2-2.png');
+		}
+
+	}
+}
